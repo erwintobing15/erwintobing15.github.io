6b75ad52b44a224deb349adacc1466062a2e4987
erwintobing15 <erwintobing15@gmail.com>
2021-11-03 13:32:46 +0700

    Customize resume image to fit on desktop and mobile screen

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,12 +1,45 @@
 import * as React from "react"
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
+import downloadFile from '../downloads/file.pdf'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { resume } from './resume.module.css'
 
-const ResumePage = () => {
+
+const ResumePage = ({ data }) => {
   return (
     <Layout pageTitle="Resume | ">
-      <h4>Resume Page</h4>
+      <Row style={{ textAlign: 'center' }}>
+        <Col xs={12} md={12} className={resume}>
+          <Img fluid={data.resumeImage.childImageSharp.fluid}/>
+        </Col>
+        <Col xs={12} md={12}>
+          <div style={{ marginTop: '1rem' }}>
+            <a href={downloadFile} download="resume" style={{ fontDecoration: 'none' }}>
+              <Button variant="warning">
+                <strong>Download</strong>
+              </Button>{' '}
+            </a>
+          </div>
+        </Col>
+      </Row>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    resumeImage: file(relativePath: { eq: "resume.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 612, maxHeight: 792, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default ResumePage
