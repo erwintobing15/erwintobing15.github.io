5b85632ee0caf4f5204c062cda947b808ce9302e
erwintobing15 <erwintobing15@gmail.com>
2021-11-05 20:57:07 +0700

    Create button detail, demo, and source code on every project screenshoot

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import {
   row,
   col,
@@ -25,6 +26,23 @@ const IndexPage = ({ data }) => {
               <div className={projectImageDesc}>
                 <h2>Dota2Web</h2>
                 <p>Recommendation System | Match Prediction |  Website  <br/> Django | Neural Network | Association Rules</p>
+                <Link to='/'>
+                  <Button variant="outline-light">
+                    <strong>Detail</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://github.com/erwintobing15/dota2web'>
+                  <Button variant="outline-light">
+                    <strong>Live Demo</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://github.com/erwintobing15/dota2web'>
+                  <Button variant="outline-light">
+                    <strong>Source Code</strong>
+                  </Button>
+                </Link>
               </div>
             </div>
           </Card>
@@ -36,12 +54,29 @@ const IndexPage = ({ data }) => {
               <div className={projectImageDesc}>
                 <h2>Daimatus</h2>
                 <p>Learning Platform | Fullstack Website | PHP Native <br/> HTML | CSS | JS | Bootstrap</p>
+                <Link to='/'>
+                  <Button variant="outline-light">
+                    <strong>Detail</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://daimatus.000webhostapp.com/'>
+                  <Button variant="outline-light">
+                    <strong>Live Demo</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://github.com/erwintobing15/daimatus'>
+                  <Button variant="outline-light">
+                    <strong>Source Code</strong>
+                  </Button>
+                </Link>
               </div>
             </div>
           </Card>
         </Col>
       </Row>
-      <Row className={row}>
+      <Row className={row} style={{ marginTop:'1rem'}}>
         <Col xs={12} md={6} className={col}>
           <Card>
             <div className={projectImage}>
@@ -49,6 +84,23 @@ const IndexPage = ({ data }) => {
               <div className={projectImageDesc}>
                 <h2>Deteksi Pantun Puisi</h2>
                 <p>Website | Django | Sentiment Analysis | SVM</p>
+                <Link to='/'>
+                  <Button variant="outline-light">
+                    <strong>Detail</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://deteksipantunpuisi.pythonanywhere.com/'>
+                  <Button variant="outline-light">
+                    <strong>Live Demo</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://github.com/erwintobing15/deteksi_pantun_puisi'>
+                  <Button variant="outline-light">
+                    <strong>Source Code</strong>
+                  </Button>
+                </Link>
               </div>
             </div>
           </Card>
@@ -60,6 +112,23 @@ const IndexPage = ({ data }) => {
               <div className={projectImageDesc}>
                 <h2>C++ Analyzer</h2>
                 <p>Generate tokens from C++ object language <br /> Website | PHP Native | Bootstrap</p>
+                <Link to='/'>
+                  <Button variant="outline-light">
+                    <strong>Detail</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://cppanalyzer.000webhostapp.com/'>
+                  <Button variant="outline-light">
+                    <strong>Live Demo</strong>
+                  </Button>
+                </Link>
+                {' '}
+                <Link to='https://github.com/erwintobing15/cpp-analyzer'>
+                  <Button variant="outline-light">
+                    <strong>Source Code</strong>
+                  </Button>
+                </Link>
               </div>
             </div>
           </Card>
@@ -71,7 +140,7 @@ const IndexPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    deteksiImage: file(relativePath: { eq: "deteksi.PNG" }) {
+    deteksiImage: file(relativePath: { eq: "deteksi.png" }) {
       childImageSharp {
         fluid(maxWidth: 1500, quality: 100) {
           ...GatsbyImageSharpFluid
@@ -79,7 +148,7 @@ export const query = graphql`
       }
     }
     dotaImage: file(
-      relativePath: { eq: "dota2web.PNG" }
+      relativePath: { eq: "analyzer.png" }
     ) {
       childImageSharp {
         fluid(maxWidth: 1500, quality: 100) {
