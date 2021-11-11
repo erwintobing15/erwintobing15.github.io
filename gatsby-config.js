ce0de7c69fea54b947ca6351101e42b431b2d9d9
erwintobing15 <erwintobing15@gmail.com>
2021-11-11 18:51:22 +0700

    Create folder images in every mdx project directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,19 @@ module.exports = {
         path: `${__dirname}/project`,
       }
     },
+    `gatsby-remark-images`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
   ],
 };
