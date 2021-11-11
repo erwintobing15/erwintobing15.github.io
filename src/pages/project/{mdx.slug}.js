ab5e1e14dd258fa96f090c7d22614925d41e653f
erwintobing15 <erwintobing15@gmail.com>
2021-11-11 15:38:33 +0700

    Create project list in bottom of project detail pages for navigation

--- src/pages/project/{mdx.slug}.js
+++ src/pages/project/{mdx.slug}.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
-import { projectStyle  } from './{mdx.slug}.module.css'
+import { projectStyle, listItem } from './{mdx.slug}.module.css'
 
 
 const ProjectDetail = ({ data }) => {
@@ -12,6 +12,18 @@ const ProjectDetail = ({ data }) => {
         <MDXRenderer >
              {data.mdx.body}
         </MDXRenderer>
+
+        <br /><br /><hr /><br /><br />
+
+        <h4>Project List</h4>
+        <ul >
+          <li><Link to='/project/disposisi' className={listItem}>Disposisi</Link></li>
+          <li><Link to='/project/dotamagus' className={listItem}>Dotamagus</Link></li>
+          <li><Link to='/project/daimatus' className={listItem}>Daimatus</Link></li>
+          <li><Link to='/project/analyzer' className={listItem}>C++ Analyzer</Link></li>
+          <li><Link to='/project/deteksi' className={listItem}>Deteksi Pantun Puisi</Link></li>
+          <li><Link to='/project/ikuliah' className={listItem}>iKuliah</Link></li>
+        </ul>
       </div>
     </Layout>
   )
